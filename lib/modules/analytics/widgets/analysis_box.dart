a1867ff783069a3a7af4560517ae962623a5b8f1
EbrahimMShaaban <ebrahimmsh2019@gmail.com>
2023-03-14 21:11:08 +0200

    small analytics card finish

--- lib/modules/analytics/widgets/analysis_box.dart
+++ lib/modules/analytics/widgets/analysis_box.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:steps/style/colors.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 
 import '../../../style/costraints.dart';
@@ -17,44 +18,93 @@ class AnalysisBox extends StatelessWidget {
         height: MediaQueryHelper.sizeFromHeight(context, 3.5),
         //  width: MediaQueryHelper.sizeFromWidth(context, 3),
         decoration: BoxDecoration(
-            color: Colors.grey, borderRadius: BorderRadius.circular(20)),
+          //  color: Colors.grey,
+            borderRadius: BorderRadius.circular(20)),
 
         child: Row(
           children: [
             SizedBox(
               width: MediaQueryHelper.sizeFromWidth(context, 5),
               height: 200,
-              child: Column(
-                //crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  const Text('Daily revenue',
-                      style: AppTextStyles.analysis_titles),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Text('45.24', style: AppTextStyles.titles),
-                  Text('+12% From week',
-                      style: Theme.of(context).textTheme.caption)
-                ],
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Text('Daily revenue',
+                        style: AppTextStyles.analysis_titles
+                            .copyWith(color: AppColor.blacktext)),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      //  mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text('45.24',
+                            style: AppTextStyles.titles.copyWith(
+                                fontSize: 40, fontWeight: FontWeight.bold)),
+                        Text(
+                          'kWh',
+                          style: AppTextStyles.box_icons.copyWith(
+                              color: AppColor.blacktext,
+                              fontWeight: FontWeight.w300),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(right: 5),
+                          child: Image.asset('assets/images/up.png',
+                              color: AppColor.blueColor),
+                        ),
+
+                        RichText(
+                          text: TextSpan(
+                              //  style: AppTextStyles.analysis_titles,
+                              //    text: "+12%",
+                              children: [
+                                TextSpan(
+                                  style: AppTextStyles.w500
+                                      .copyWith(color: AppColor.blueColor),
+                                  text: "+12%",
+                                ),
+                                TextSpan(
+                                    style: AppTextStyles.box_icons.copyWith(
+                                        color: AppColor.blacktext,
+                                        fontWeight: FontWeight.w300),
+                                    text: " Today"),
+                              ]),
+                        ),
+
+                        // Text('+12% Today',
+                        //     style: Theme.of(context).textTheme.caption),
+                      ],
+                    )
+                  ],
+                ),
               ),
             ),
-            SizedBox(
+            Container(
+              // color: Colors.red,
               width: MediaQueryHelper.sizeFromWidth(context, 5),
               height: 200,
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.end,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   Text(
                     "Nov 29, 02:29 AM",
-                    style: AppTextStyles.date,
+                    style: AppTextStyles.box_icons.copyWith(
+                        color: AppColor.blacktext, fontWeight: FontWeight.w300),
                   ),
                   SizedBox(
                       height: 150,
                       child: SfCartesianChart(
                           backgroundColor: Colors.purpleAccent,
                           primaryXAxis: CategoryAxis(),
-
                           series: <LineSeries<SalesData, String>>[
                             LineSeries<SalesData, String>(
                                 // Bind data source
---
+++ lib/modules/analytics/widgets/large_analytics.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class LAnalytics extends StatelessWidget {
+  const LAnalytics({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
